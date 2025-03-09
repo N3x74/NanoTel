116ed7c20557d0b61b1534041f60db6fd3adc741
hossein <4.ho3ein@gmail.com>
2025-03-09 21:32:52 +0330

    feat: add HttpClient to src/NanoTel.php

--- src/NanoTel.php
+++ src/NanoTel.php
@@ -4,15 +4,22 @@ namespace NanoTel;
 
 use InvalidArgumentException;
 use SensitiveParameter;
+use NanoTel\Http\HttpClient;
 
 class NanoTel {
-    private string $botToken;
+    public string $baseUrl = "https://api.telegram.org/bot";
+
+    private ?string $botToken;
+
+    private HttpClient $client;
 
     public function __construct(#[SensitiveParameter] ?string $botToken = null) {
         if (!$botToken) {
             throw new InvalidArgumentException("Bot token is required");
         }
         $this->botToken = $botToken;
+        $this->baseUrl .= $botToken;
+        $this->client = new HttpClient($this->baseUrl);
     }
 
     public function getBotToken(): string {
